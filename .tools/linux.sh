699b48476d2c6402207eb349ebebc20b77356ae9
Peter Kalambet <peter@kalambet.dev>
2020-03-18 10:35:06 +0300

    add nvm startup configuration

--- .tools/linux.sh
+++ .tools/linux.sh
@@ -5,4 +5,18 @@ if [ -f ~/Developer/shell/exercism_completion.zsh ]; then
     source ~/Developer/shell/exercism_completion.zsh ; 
 fi
 
+# Initialize brew for linux
+export PATH="/home/linuxbrew/.linuxbrew/bin":$PATH
+
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+if [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ]; then
+	source "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"
+fi
+
+# This loads nvm bash_completion
+if [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ]; then
+	source "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"
+fi
+
 ### Linux end ###
