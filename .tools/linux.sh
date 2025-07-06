c3acc20ba48a69a70a7474cad4ae29c88d330f90
Peter Kalambet <peter@kalambet.dev>
2025-07-06 20:31:59 +0200

    update linux gitconfig

--- .tools/linux.sh
+++ .tools/linux.sh
@@ -1,7 +1,7 @@
 ### Linux start ###
 
 # Global gitconfig setup
-export GIT_CONFIG_GLOBAL=$HOME/.tools/gitconfig-macos
+export GIT_CONFIG_GLOBAL=$HOME/.tools/gitconfig-linux
 
 # zsh-autosuggestions activation
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -12,13 +12,13 @@ alias cat="bat"
 
 # Rust setup
 if [ -f $HOME/.cargo/env ]; then
-    source $HOME/.cargo/env;
+  source $HOME/.cargo/env
 fi
 
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 ### Linux end ###
